70b789c1180c661e905b8bf30856a463618e6849
AlexHuynh23 <70555816+AlexHuynh23@users.noreply.github.com>
2021-01-26 00:49:07 -0800

    Variable for position of yellow goal and angle to get to goal.  Deactivated b2, b3, y1, y2, y3

--- controllers/rcj_soccer_player_b1/rcj_soccer_player_b1.py
+++ controllers/rcj_soccer_player_b1/rcj_soccer_player_b1.py
@@ -18,22 +18,28 @@ class MyRobot(rcj_soccer_robot.RCJSoccerRobot):
                 robot_pos = data[self.name]
                 # Get the position of the ball
                 ball_pos = data['ball']
+                ballDistance = absolute(robot_pos-ball_pos)
+                goalPosition = {'x':-0.75,'y':0}
 
                 # Get angle between the robot and the ball
                 # and between the robot and the north
                 ball_angle, robot_angle = self.get_angles(ball_pos, robot_pos)
+                goal_angle, robot_angle = self.get_angles(goalPosition, robot_pos)
 
                 # Compute the speed for motors
                 direction = utils.get_direction(ball_angle)
+                direction2 = utils.get_direction(goal_angle)
 
                 # If the robot has the ball right in front of it, go forward,
                 # rotate otherwise
+
                 if direction == 0:
                     left_speed = -5
                     right_speed = -5
                 else:
                     left_speed = direction * 4
                     right_speed = direction * -4
+
 
                 # Set the speed to motors
                 self.left_motor.setVelocity(left_speed)
